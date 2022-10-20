3164f027a06a8d97c317832a090cb0331e5dc8d6
Yowil-Amezquita26 <yowilamezquita380@hotmail.com>
2022-10-20 09:14:31 -0400

    removed / from origin in corsoption

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ var cors = require('cors')
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 var corsOptions = {
-  origin: 'http://127.0.0.1:5173/',
+  origin: 'http://127.0.0.1:5173',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
