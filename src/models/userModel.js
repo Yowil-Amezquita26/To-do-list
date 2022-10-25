1ae06804d7fa5475cbaa3336fdab43e1481bb3e5
Yowil-Amezquita26 <yowilamezquita380@hotmail.com>
2022-10-25 10:46:01 -0400

    fixing usermodel and newticket controller

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,27 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: String,
-    last_name: String,
-    password: { type: String },
-    email: {
-        type: String,
-        require: true,
-        index: true,
-        unique: true,
-        sparse: true,
-    },
-    signUpDate: { type: Date, default: Date.now() },
-    ticket: [
+  name: String,
+  last_name: String,
+  password: { type: String },
+  email: {
+    type: String,
+    require: true,
+    index: true,
+    unique: true,
+    sparse: true,
+  },
+  signUpDate: { type: Date, default: Date.now() },
+  ticket: 
+    {
+      not_done: [
         {
-        title: { type: String},
-        desciption: { type: String},
-        status: { type: String}
-      },
-    ]
-})
+          title: { type: String },
+          desciption: { type: String },
+          status: { type: String },
+        },
+      ],
 
-const User = mongoose.model('User', userSchema);
+      doing: [
+        {
+          title: { type: String },
+          desciption: { type: String },
+          status: { type: String },
+        },
+      ],
+      done: [
+        {
+          title: { type: String },
+          desciption: { type: String },
+          status: { type: String },
+        },
+      ],
+    },
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- src/router/controllers/newTicket.js
+++ src/router/controllers/newTicket.js
@@ -3,14 +3,38 @@ const User = require('../../models/userModel')
 async function newTicket(req,res){
     const userId = req.params.userId
     const body = req.body
-    console.log(body)
-    await User.findOneAndUpdate({
-        _id:  userId,
-    },
-    {
-        $push:{ticket:body}
-    })
-    res.json({messaje:"Ticket Added"})
+    const status = body.status
+    console.log(body.status)
+    
+    if(status == "not done") {
+        
+        await User.findOneAndUpdate({
+            _id:  userId,
+        },
+        {
+            $push:{'ticket.not_done':body}
+        })
+    };
+    if(status == "doing") {
+        
+        await User.findOneAndUpdate({
+            _id:  userId,
+        },
+        {
+            $push:{'ticket.doing':body}
+        })
+    };
+    if(status == "done") {
+        
+        await User.findOneAndUpdate({
+            _id:  userId,
+        },
+        {
+            $push:{'ticket.done':body}
+        })
+    };
+
+    res.json({messaje:`Ticket Added`,})
     
 }
 
