39f30641b872e3e63c901a39f48753ae8fb8f012
Yowil-Amezquita26 <yowilamezquita380@hotmail.com>
2022-10-31 10:28:45 -0400

    adding a messaje for when user exist

--- src/router/controllers/registerNewUser.js
+++ src/router/controllers/registerNewUser.js
@@ -3,7 +3,7 @@ const User = require('../../models/userModel')
 async function registerNewUser(req, res){
     const body = req.body
 
-    User.create(body).then((data) => {res.json(data), {messaje: 'User register' } }).catch((error) => res.json(error))
+    User.create(body).then((data) => {res.json(data), {messaje: 'User register' } }).catch((error) => res.json({error, messaje: 'User Already Exist'}))
 }
 
 module.exports = registerNewUser
