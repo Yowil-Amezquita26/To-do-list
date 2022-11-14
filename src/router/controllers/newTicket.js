7dced6ea1a3379dbcda2a3de6a30b53fa7dd5c0b
Yowil-Amezquita26 <yowilamezquita380@hotmail.com>
2022-11-14 14:46:49 -0500

    fixing some issues

--- src/router/controllers/newTicket.js
+++ src/router/controllers/newTicket.js
@@ -3,15 +3,13 @@ const User = require('../../models/userModel')
 async function newTicket(req,res){
     const userId = req.params.userId
     const body = req.body
-    const status = body.status
-    console.log(body.status)
     
     console.log(body)
     await User.findOneAndUpdate({
         _id:  userId,
     },
     {
-        $push:{ticket:body}
+        $push:{tickets:body}
     })
     res.json({messaje:"Ticket Added"})
     
